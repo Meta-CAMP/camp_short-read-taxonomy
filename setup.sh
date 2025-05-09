f9b4e326b65d4a30997a16ebd7fa532210129707
Lauren Mak <37590237+lauren-mak@users.noreply.github.com>
2025-05-09 14:21:14 -0400

    Patch find_install_conda_env

--- setup.sh
+++ setup.sh
@@ -51,6 +51,7 @@ find_install_conda_env() {
         echo "🚀 Installing $1 in $DEFAULT_CONDA_ENV_DIR/$1..."
         conda create --prefix $DEFAULT_CONDA_ENV_DIR/$1 -c conda-forge -c bioconda $1
         echo "✅ $1 installed successfully!"
+    fi
 }
 
 # Install databases in the specified directory
